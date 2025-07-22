91fe698d60875dbf814a51a9f3d7fc5e84b72720
hungrypicklemonster <lisadinh455@gmail.com>
2025-07-22 15:22:19 -0700

    worked on calendar

---
+++ web/src/app/calendar.tsx
@@ -0,0 +1,41 @@
+import { Calendar } from "@/components/ui/calendar"
+import React from "react";
+
+function CalendarPage() {
+    const today = new Date(); 
+    const day = today.getDate();
+    const week = today.getDay();
+
+    const DaysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+return (
+  <div className="flex items-start gap-4 p-4">
+  
+    <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-lg border" /><div>
+
+            <div className="text-lg font-semibold">
+                <h1>{DaysOfWeek[week]}, {day}</h1>
+            </div>
+            <div className=" border-2 p-2 w-400">
+                {Array.from({ length: 24 }, (_, i) => i + 1).map((hour) => {
+                    return (
+                        <div className=" text-black border-2 w-full my-2 p-4">
+                            {hour}
+                            {hour > 12 ? 'PM' : 'AM'}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+        </div>
+    )
+
+
+}
+
+export default CalendarPage; 
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Upload } from "./app/file-upload/upload";
 import App from "./App";
+import Calendar from "./app/calendar";
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -10,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route index element={<Upload />} />
         <Route path="/app" element={<App />} />
+        <Route path="/cal" element={<Calendar/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
